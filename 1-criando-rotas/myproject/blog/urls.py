6d43f223b34e823d77c445a98146fa5f8ce5cee8
Matheus Battisti <matheusbudkewicz@gmail.com>
2018-12-15 13:21:06 -0200

    finalizando artigo 1

--- 1-criando-rotas/myproject/blog/urls.py
+++ 1-criando-rotas/myproject/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('sobre-nos/', views.about, name='about'),
+    path('contato/', views.contact, name='contact'),
 ]
--- 1-criando-rotas/myproject/blog/views.py
+++ 1-criando-rotas/myproject/blog/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def post_list(request):
 	return HttpResponse('Olá Mundo!');
+
+def about(request):
+	return HttpResponse('Sobre nós.')
+
+def contact(request):
+	return HttpResponse('Página de contato.')
