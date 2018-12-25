75e6d3f3a1b103e485b574e027e9e83d431cb9b0
Matheus Battisti <matheusbudkewicz@gmail.com>
2018-12-25 18:44:17 -0200

    finalizando post admin

---
+++ 5-admin/myproject/blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display  = ('title','author', 'created_at', 'updated_at', 'status')
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
