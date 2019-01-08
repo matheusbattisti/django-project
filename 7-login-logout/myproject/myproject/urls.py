9223c9d67b7adb8e3f60284931ec047427b95126
Matheus Battisti <matheus.battisti@essentia.com.br>
2019-01-08 11:55:10 -0200

    login finalizado

---
+++ 7-login-logout/myproject/myproject/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+	path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
+]
