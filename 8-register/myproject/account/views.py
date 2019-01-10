9c9601528ba91335a21d0a8d331c27c5f687d4ff
Matheus Battisti <matheus.battisti@essentia.com.br>
2019-01-10 11:07:55 -0200

    finalizando register

---
+++ 8-register/myproject/account/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+class register(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'account/register.html'
