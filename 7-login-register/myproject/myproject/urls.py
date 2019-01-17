bef71dd846078617f45fbe79a3504e9511c46e31
Matheus Battisti <matheusbudkewicz@gmail.com>
2019-01-16 22:57:34 -0200

    postgres

--- 7-login-register/myproject/myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-	path('', include('blog.urls')),
-    path('admin/', admin.site.urls),
-]
--- 9-django-e-postgresql/myproject/myproject/settings.py
+++ 9-django-e-postgresql/myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME', 'projeto'),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASS', 'moita123'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
